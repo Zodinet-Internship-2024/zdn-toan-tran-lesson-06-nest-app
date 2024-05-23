016bd91718e87df7cc77d6b0ef366d7e2abecb16
zdn-toan-tran <toan.tran@zodinet.tech>
2024-05-23 14:21:50 +0700

    feat: catch errors globally

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { configSwagger } from './config/swagger.config';
+import { AllExceptionsFilter } from './exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   configSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
